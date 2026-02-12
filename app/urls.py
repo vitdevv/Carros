e6a623626ffa01b469368f1fc1a8999e01e5aed7
vitdevv <vldev3333@gmail.com>
2026-02-11 23:05:17 -0300

    Add accounts app with user registration functionality and update URLs

--- app/urls.py
+++ app/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars import views
+from cars.views import cars_view, car_details, new_car_view
+from accounts.views import register_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('cars/', views.cars_view, name='cars_list'),
-    path('cars/<int:car_id>/', views.car_details, name='car_details'),
-    path('new_car/', views.new_car_view, name='new_car'),
+    path('register/', register_view, name='register'),
+    path('cars/', cars_view, name='cars_list'),
+    path('cars/<int:car_id>/', car_details, name='car_details'),
+    path('new_car/', new_car_view, name='new_car'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
